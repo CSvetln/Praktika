e141a6134f826d270ef943ef159ea2b34d548f58
Светлана Сорокина <cveta2500@outlook.com>
2019-10-11 22:56:06 +0700

    Документ сохраняется в поток

--- DutyOfServiceDepart/SendSchedule/SendingMailRu.cs
+++ DutyOfServiceDepart/SendSchedule/SendingMailRu.cs
@@ -1,12 +1,9 @@
 ﻿using DutyOfServiceDepart.SendSchedule;
 using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.IO;
 using System.Net;
 using System.Net.Mail;
-using System.Net.Mime;
-using System.Text.RegularExpressions;
-using System.Web;
+
 
 namespace DutyOfServiceDepart.Mail
 {
@@ -26,10 +23,11 @@ namespace DutyOfServiceDepart.Mail
 			Message.To.Add(new MailAddress(Email));
 			Message.Subject = Subject;
 			Message.Body = Body;
-			string File = Schedule.GetSchedule(date);
-			Message.Attachments.Add(new Attachment(File));
-
-			Smtp.Send(Message);
+			using (MemoryStream stream = Schedule.GetSchedule(date))
+			{
+				Message.Attachments.Add(new Attachment(stream, "График.xlsx"));
+				Smtp.Send(Message);
+			}
 			Message.Dispose();
 			Smtp.Dispose();		
 		}
