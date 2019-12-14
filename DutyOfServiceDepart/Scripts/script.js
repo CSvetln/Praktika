05d169b1ea1f7927302c54b59395f5d508433f45
Светлана Сорокина <cveta2500@outlook.com>
2019-12-14 23:29:58 +0700

    скрипт

--- DutyOfServiceDepart/Scripts/script.js
+++ DutyOfServiceDepart/Scripts/script.js
@@ -1,9 +1,8 @@
 ﻿function funcEdit(id) {
     var divsToHide = document.getElementsByClassName("pencil");
     for (var i = 0; i < divsToHide.length; i++) {
-        divsToHide[i].style.visibility = "hidden"; 
+        divsToHide[i].style.visibility = "hidden";
     }
-    document.querySelectorAll(".my div").style.visibility = "block";
+    document.getElementById(id).className = "block";
     return false;
 }
-
